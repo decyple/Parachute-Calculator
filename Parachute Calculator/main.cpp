63c4407ce1126613ac0da7aed9a94c81a7e09f80
Mark McCorkle <markamccorkle@yahoo.com>
2019-07-21 11:08:34 -0500

    Initial Commit

---
+++ Parachute Calculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Parachute Calculator
+//
+//  Created by Mark McCorkle on 7/21/19.
+//  Copyright © 2019 Mark McCorkle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
